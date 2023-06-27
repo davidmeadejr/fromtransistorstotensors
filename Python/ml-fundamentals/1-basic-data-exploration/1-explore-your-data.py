27eda6b6fef81e4cfebf163a57606447145ddc69
David Meade Jr <davidmeadejnr@gmail.com>
2023-06-27 14:24:34 +0100

    Basic data exploration section complete.

---
+++ Python/ml-fundamentals/1-basic-data-exploration/1-explore-your-data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Path of the file to read
+iowa_file_path = "../input/home-data-for-ml-course/train.csv"
+
+# Fill in the line below to read the file into a variable home_data
+home_data = pd.read_csv(iowa_file_path)
---
+++ Python/ml-fundamentals/1-basic-data-exploration/2-review-the-data.py
@@ -0,0 +1,8 @@
+# Print summary statistics in next line
+home_data.describe()
+
+# What is the average lot size (rounded to nearest integer)?
+avg_lot_size = 10517
+
+# As of today, how old is the newest home (current year - the date in which it was built)
+newest_home_age = 13
